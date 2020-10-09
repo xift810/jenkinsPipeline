fc6d2738304dfa1d3c0e4b36a5e580495bcffc5c
unknown <xhai8612@gmail.com>
2020-10-09 17:07:45 +0800

    playgroundpython fix bug

--- k12playgroundPython.groovy
+++ k12playgroundPython.groovy
@@ -2,8 +2,8 @@ pipeline {
     agent {label 'k12-win12-001'}
     
     environment {
-        CI_PROJECT_DIR = 'E:\\k12jenkins\\playgroundPython'
-        Unity_Editor = 'C:\\Program Files\\Unity2019.3.1f1\\Editor\\Unity.exe'
+        CI_PROJECT_DIR = 'E:\\k12jenkins\\python\\playground'
+        Unity_Editor = 'C:\\Program Files\\Unity2019.4.8f1\\Editor\\Unity.exe'
         // CI_COMMIT_TAG = 'python_1.0.0'
         CI_COMMIT_MESSAGE = 'test_MESSAGE_1.0.16'
         Scripts_Folder = 'E:\\ci_scripts'
@@ -35,6 +35,9 @@ pipeline {
         stage('Mac export package') {
            steps {
                powershell """
+                    cd $CI_PROJECT_DIR
+                    git reset --hard HEAD
+                    git clean -fx
                     cd $CI_PROJECT_DIR\\ci
                     .\\windows-build.ps1  "$Unity_Editor" "$CI_PROJECT_DIR" "$CI_COMMIT_TAG" "$Scripts_Folder" UNITY_K12_PLAYGROUND_INT osx
                 """
@@ -44,10 +47,10 @@ pipeline {
         stage('UploadPlugin'){
             steps {
                 powershell """
-                    python $Scripts_Folder\\downloadfile.py --fileName=PlaygroundLib/Launcher/Temp/Unity.InternalAPIEditorBridge.001.dll --dest=$CI_PROJECT_DIR/Unity.InternalAPIEditorBridge.001.dll
-                    python $Scripts_Folder\\uploadfile.py --uploadPrefix=PlaygroundLib/Launcher/$CI_COMMIT_TAG --filePath=$CI_PROJECT_DIR/Unity.InternalAPIEditorBridge.001.dll --isPublic=True
-                    python $Scripts_Folder\\downloadfile.py --fileName=PlaygroundPlugin/Temp/UnityK12Playground_windows.unitypackage --dest=$CI_PROJECT_DIR/UnityK12Playground_windows.unitypackage
-                    python $Scripts_Folder\\uploadfile.py --uploadPrefix=PlaygroundPlugin/$CI_COMMIT_TAG --filePath=$CI_PROJECT_DIR/UnityK12Playground_windows.unitypackage --isPublic=True
+                    python $Scripts_Folder\\downloadfile.py --fileName=Python/PlaygroundLib/Launcher/Temp/Unity.InternalAPIEditorBridge.001.dll --dest=$CI_PROJECT_DIR/Unity.InternalAPIEditorBridge.001.dll
+                    python $Scripts_Folder\\uploadfile.py --uploadPrefix=Python/PlaygroundLib/Launcher/$CI_COMMIT_TAG --filePath=$CI_PROJECT_DIR/Unity.InternalAPIEditorBridge.001.dll --isPublic=True
+                    python $Scripts_Folder\\downloadfile.py --fileName=Python/PlaygroundPlugin/Temp/UnityK12Playground_windows.unitypackage --dest=$CI_PROJECT_DIR/UnityK12Playground_windows.unitypackage
+                    python $Scripts_Folder\\uploadfile.py --uploadPrefix=Python/PlaygroundPlugin/$CI_COMMIT_TAG --filePath=$CI_PROJECT_DIR/UnityK12Playground_windows.unitypackage --isPublic=True
                     python $Scripts_Folder\\downloadfile.py --fileName=Python/PlaygroundPlugin/Temp/UnityK12Playground_osx.unitypackage --dest=$CI_PROJECT_DIR/UnityK12Playground_osx.unitypackage
                     python $Scripts_Folder\\uploadfile.py --uploadPrefix=Python/PlaygroundPlugin/$CI_COMMIT_TAG --filePath=$CI_PROJECT_DIR/UnityK12Playground_osx.unitypackage --isPublic=True
                     python $Scripts_Folder\\uploadfile.py --uploadPrefix=Python/PlaygroundPluginRaw/$CI_COMMIT_TAG --filePath=E:/PlaygroundAssetBundleCITemp/Package/UnityK12PlaygroundRaw_windows.unitypackage               
